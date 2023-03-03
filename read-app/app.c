66baf590cc3fe13c9642935d00c99f6c847a059d
Ahmedhussien91 <a.hussien.91@gmail.com>
2023-03-03 08:37:02 +0200

    add printing demon + build application statically

--- read-app/app.c
+++ read-app/app.c
@@ -5,6 +5,14 @@
 #include "file.h"
 
 
+void printing_demon() {
+	while(1) {
+		printf("printing_demon is up and running");
+		sleep(10);
+		}
+}
+
+
 int main() {
     // open threads for each task
     puts("Making threadpool with 4 threads");
@@ -13,6 +21,8 @@ int main() {
     // thread to read from file
     thpool_add_work(thpool, stdinRead, (char *)"stdinRead");
     thpool_add_work(thpool, open_read_file, (char *)"filename");
+    
+    thpool_add_work(thpool, printing_demon, (char *)"filename");
 
     // thread to read from socket 
 
